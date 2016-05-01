7611789b02dec0c73315f6c636b43fbdb13b63a5
Mohamed ayyad <moh.ayad93@hotmail.com>
2016-05-01 23:25:58 +0200

    working on freind activty

--- YallaNotlob/app/controllers/home_controller.rb
+++ YallaNotlob/app/controllers/home_controller.rb
@@ -3,8 +3,14 @@ class HomeController < ApplicationController
 
   	if current_user
 		@orders = Order.all.where(user_id: current_user.id).order(created_at: :desc) 
-		puts @orders
-    else
+		# --------------------------
+
+		# @all = Order.joins(:user , :friendship).select("*").where(friendship: {user_id: current_user.id}).where(friendship: {friend_id: 2})
+		
+		@all = Order.joins(:user).select("*")
+
+
+	else
     	redirect_to '/users/sign_in'
     end
   end
