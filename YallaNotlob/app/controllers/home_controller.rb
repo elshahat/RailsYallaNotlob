fd40db09d8640a82b419f60f227d1c6bec679980
Mohamed ayyad <moh.ayad93@hotmail.com>
2016-05-01 17:29:00 +0200

    finshied half of homepage

--- YallaNotlob/app/controllers/home_controller.rb
+++ YallaNotlob/app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
 
   	if current_user
-		@orders = Order.all.where(user_id: current_user.id) 
+		@orders = Order.all.where(user_id: current_user.id).order(created_at: :desc) 
 		puts @orders
     else
     	redirect_to '/users/sign_in'
