423b2a8a0e4111f33a5df1fa424ce6a928230023
Hazem Taha <thatskan@gmail.com>
2016-05-02 02:47:50 +0200

    add - menu image now avaliable in order details page & more info is displayed

--- YallaNotlob/app/models/order.rb
+++ YallaNotlob/app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ActiveRecord::Base
   has_many :items, :dependent => :delete_all
   has_many :order_invites, :dependent => :delete_all
   enum status: { Waiting: 1, Finished: 0}
-  enum type: { Breakfast: 1, Lunch: 2, Dinner: 3}
+  enum order_type: { Breakfast: 1, Lunch: 2, Dinner: 3}
   mount_uploader :menu_img, ImageUploader
   validates :destination, presence: true
   validates :menu_img, presence: true
